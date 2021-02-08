f336e295bc6c814e335e1fdf1366037f383e070a
nsherman113 <noahsherman113@gmail.com>
2021-02-07 19:02:04 -0600

    error handling inputted

--- module-6-starter/assets/js/single.js
+++ module-6-starter/assets/js/single.js
@@ -1,8 +1,23 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
-var getRepoIssues = function(repo) {
+var getRepoName = function() {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  if(repoName) {
+    // display repo name. 
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  }else {
+    // if a repo did not render, redirect to homepage. 
+    document.location.replace("./index.html");
+  }
+}
+
+
+var getRepoIssues = function(repo,repoName) {
   // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
@@ -20,8 +35,7 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      console.log(response);
-      alert("There was a problem with your request!");
+      document.location.replace("./index.html");
     }
   });
 };
@@ -81,4 +95,4 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
